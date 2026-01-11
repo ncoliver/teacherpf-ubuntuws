6250afb03aa4686edeb87748386ba1e422738ceb
ncmoliver <mrmarquesoliver@gmail.com>
2026-01-11 13:10:39 -0500

    project dep update

--- teacherpf-ubuntu/wsgi.py
+++ teacherpf-ubuntu/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teacherportfolio.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'teacherpf-ubuntu.settings')
 
 application = get_wsgi_application()
