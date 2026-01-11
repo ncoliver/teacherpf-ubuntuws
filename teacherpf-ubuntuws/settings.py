1aa91a5fe1388257de95991b711b41507c50c17a
ncmoliver <mrmarquesoliver@gmail.com>
2026-01-11 17:24:08 -0500

    project settings updated

--- teacherpf-ubuntuws/settings.py
+++ teacherpf-ubuntuws/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'teacherportfolio.urls'
+ROOT_URLCONF = 'teacherpf-ubuntuws.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'teacherportfolio.wsgi.application'
+WSGI_APPLICATION = 'teacherpf-ubuntuws.wsgi.application'
 
 
 # Database
